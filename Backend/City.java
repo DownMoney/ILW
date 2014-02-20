8e9e63e865a74e58ca67d426c1decff820e08559
koenraat <konrad@stojanowski.eu>
2014-02-20 14:01:48 +0000

    getAllTrans - almost done
    needs airport->code change

--- Backend/City.java
+++ Backend/City.java
@@ -7,10 +7,14 @@ public class City {
 	Vector<Transport> inTrans, outTrans;
 	TimeDate time;
 	long pop;
+	int type; //0 = airport, 1 = country
 	Vector<String> airports;
 	public City(String name)
 	{
-		this.name = name;
+		this.name = name.split(" ")[0];
+		this.events = null;
+		this.inTrans = null;
+		this.outTrans = null;
 	}
 	public boolean equals(City c){
 		return name.equals(c.name);
