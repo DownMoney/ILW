e2a8266e0a5a98283bb51d2302d596952239d4a4
DownMoney <michael@lotkowscy.co.uk>
2014-02-20 16:08:48 +0000

    m

--- api/events.php
+++ api/events.php
@@ -1,6 +1,6 @@
 <?php
 
-$fileContents = file_get_contents('http://api.eventful.com/rest/events/search?location='.$_GET['city'].'&within=15&sort_order=popularity&app_key=vd93CbRJDN8TgsPG');
+$fileContents = file_get_contents('http://api.eventful.com/rest/events/search?location='.$_GET['city'].'&within=50&app_key=vd93CbRJDN8TgsPG');
 $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
 $fileContents = trim(str_replace('"', "'", $fileContents));
 $xml = simplexml_load_string($fileContents);
