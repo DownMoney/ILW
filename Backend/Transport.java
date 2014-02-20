a424a4bf55691fa2a529b0f0961c9a4a14002073
ed728 <edvin.jaras@gmail.com>
2014-02-20 00:17:53 +0000

    Update Transport.java

--- Backend/Transport.java
+++ Backend/Transport.java
@@ -1,11 +1,27 @@
-
 public class Transport {
 	TimeDate out, in;
 	City start, end;
 	TransType type;
 	double cost;
 	String outAirport, inAirport;
-	
+	int s,e;
+ +	public Transport(City start, City end, double cost)
+ +	{
+ +		this.start = start;
+ +		this.end = end;
+ +		this.cost = cost;
+ +	}
+ +	public Transport(int start, int end, double cost)
+ +	{
+ +		this.s = start;
+ +		this.e = end;
+ +		this.cost = cost;
+ +	}
+ +	public void update(City start, City end)
+ +	{
+ +		this.start = start;
+ +		this.end = end;
+ +	}
 	public String getType(){
 		switch(type){
 			case  FLIGHT:
@@ -18,4 +34,6 @@ public class Transport {
 				return "none";
 		}
 	}
+	
+	
 }
