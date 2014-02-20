57385512713482227bfb5108347dd7d0a124196c
Edvin <edvin@edvin-SATELLITE-L855.(none)>
2014-02-20 00:27:39 +0000

    Update 5

--- Backend/Transport.java
+++ Backend/Transport.java
@@ -5,23 +5,23 @@ public class Transport {
 	double cost;
 	String outAirport, inAirport;
 	int s,e;
- +	public Transport(City start, City end, double cost)
- +	{
- +		this.start = start;
- +		this.end = end;
- +		this.cost = cost;
- +	}
- +	public Transport(int start, int end, double cost)
- +	{
- +		this.s = start;
- +		this.e = end;
- +		this.cost = cost;
- +	}
- +	public void update(City start, City end)
- +	{
- +		this.start = start;
- +		this.end = end;
- +	}
+ 	public Transport(City start, City end, double cost)
+ 	{
+ 		this.start = start;
+ 		this.end = end;
+ 		this.cost = cost;
+ 	}
+ 	public Transport(int start, int end, double cost)
+ 	{
+ 		this.s = start;
+ 		this.e = end;
+ 		this.cost = cost;
+ 	}
+ 	public void update(City start, City end)
+ 	{
+ 		this.start = start;
+ 		this.end = end;
+ 	}
 	public String getType(){
 		switch(type){
 			case  FLIGHT:
