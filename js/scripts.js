32a4a082958a20a85d177a8aa650839e91825ab0
DownMoney <michael@lotkowscy.co.uk>
2014-02-21 00:27:05 +0000

    m

--- js/scripts.js
+++ js/scripts.js
@@ -172,7 +172,7 @@ function toTimeline(cities){
 			   
 	for (var i = 0; i < cities.length; i++) {
 		name = cities[i]['FromCity'].trim();
-		name = name.split(' ')[0].toUpper();
+		name = name.split(' ')[0].toUpperCase();
 	//	if (m!=null)
 	//		name = m[0];
 		console.log(m);
