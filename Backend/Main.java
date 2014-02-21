39d7d5b4ab1f73471fe49e8b1ae664d99d4b6268
Edvin <edvin@edvin-SATELLITE-L855.(none)>
2014-02-21 01:53:12 +0000

    Backend done 2

--- Backend/Main.java
+++ Backend/Main.java
@@ -1,24 +1,26 @@
 import java.util.*;
 public class Main {
-	final static int N = 100;
+	final static int N = 5;
 	static TimeDate end, start;
 	static City out;
 	static City in;
-	double maxCost;
+	static double maxCost;
 	static User user;
 	public static void main(String[] args){
 		Search s = new Search();
 		TimeDate start = new TimeDate(10, 0, 0, 2014, 2, 21);
 		TimeDate end = new TimeDate(10, 0, 0, 2014, 2, 26);
 		long t = System.currentTimeMillis();
-		Vector<Route> r = s.findPaths(new City("Athens"), null, 600, start, end, 5, null);
+		//Vector<Route> r = s.findPaths(new City("Athens"), null, 600, start, end, 5, user);
 		//System.out.println(r.size());
-		System.out.println(JSON.toJSON(r));
+		//System.out.println(JSON.toJSON(r));
 		//for(Route ri : r){
 			//System.out.println(ri.toString());
 		//}
 		//System.out.println(System.currentTimeMillis() - t);
-		//parseArgs(args[0]); 
+		parseArgs(args[0]);
+		Vector<Route> r = s.findPaths(out, in, maxCost, start, end, N, user);
+		System.out.println(JSON.toJSON(r));
 	}
 	
 	public static void parseArgs(String args){
@@ -39,5 +41,6 @@ public class Main {
 		String[] cats = params[5].split(",");
 		String[] keys = params[6].split(",");
 		user = new User(cs, cats, keys);
+		maxCost = Double.parseDouble(params[7]);
 	}
 }
