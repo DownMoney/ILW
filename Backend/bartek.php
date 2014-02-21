4d57be1e0ed6424a3ca1fba8c787c8de1d4e8ef9
DownMoney <michael@lotkowscy.co.uk>
2014-02-21 01:53:06 +0000

    m

---
+++ Backend/bartek.php
@@ -0,0 +1,26 @@
+<?php
+	$fromcity = $_GET['from'];
+	if($_GET['to'] != ""){
+		$tocity = $_GET['to'];
+	} else {
+		$tocity = "null";
+	}
+	$startdate = date('Y-m-d', strtotime($_GET['ddate']))." 00:00:00";
+	$enddate = date('Y-m-d', strtotime($_GET['adate']))." 23:59:59";
+
+		$viacities = '';
+		$i=0;
+		while (isset($_GET['via'.$i])) {
+			$viacities = $viacities . $_GET['via'.$i] . ',';
+		}
+		$viacities = rtrim($viacities,',');
+		$category = $_GET['eventType'];
+		$keywords = $_GET['keywords'];
+		$maxmoney = $_GET['maxamount'];
+
+	$command = $fromcity . ";" . $tocity . ";" . $startdate . ";" . $enddate . ";" . $viacities . ";" . $category . ";" . $keywords . ";" . $maxmoney . ";";
+	echo 'java Main "' . $command.'"';
+	//echo shell_exec('echo hi');
+	$var = shell_exec('java Main "' . $command.'"');
+	echo $var;
+?>
